f9aafa4c32a2653c1294e8c4e3211c677f4d0a7d
Muhammad emad Hanafy <126643523+muhammademad5@users.noreply.github.com>
2025-01-25 12:37:36 +0200

    Update app1.js

--- js/app1.js
+++ js/app1.js
@@ -5,15 +5,29 @@ const forw = document.querySelector('.fa-forward-step');
 const ttl = document.querySelector('.title');
 const art_img = document.querySelector('#artist');
 const art_name = document.querySelector('#name');
-const playSong = document.querySelector('#playsong');
+const $playButton = document.querySelector('#playsong');
 const ProgressEvent = document.querySelector('.line'); 
+const lines = document.querySelector('.lineChild');
+const progress = document.querySelector('.line');
+const strt = document.querySelector('#start');
+const end = document.querySelector('#end');
 
-const artist_name = ['muhammad emad', 'ahmad khaled', 'muhammad ali'];
-const artist_title = ['Gods plan', 'The last day', 'The last night'];
+const artist_name = ['نجيب محفوظ', 'نجيب محفوظ', 'نجيب محفوظ', 'نجيب محفوظ'];
+const artist_title = ['00. العنوان', '001. مقدمة', '002. الفصل الأول الجزء الأول', '003. الفصل الأول الجزء الثاني'];
 
-playSong.addEventListener('click', effect);
+ProgressEvent.addEventListener('click', (e) => {
+    const widthbar2 = (e.offsetX / e.target.clientWidth) * ad.duration; 
+    ad.currentTime = widthbar2;
+    updateProgress();
+    updateTimer();
+});
 
-function effect() {
+$playButton.addEventListener('click', playSong);
+
+function playSong() {
+    let lineInterval = null
+    let progInterval = null
+
     if (ad.duration === ad.currentTime) {
         x += 1;
         console.log(x);
@@ -21,22 +35,20 @@ function effect() {
 
     if (!playing.classList.contains('none')) {
         ad.play();
-        setInterval(Prog, 1000); 
-        setInterval(line, 1000); 
-        ProgressEvent.addEventListener('click', (e) => {
-            const widthbar2 = (e.offsetX / e.target.clientWidth) * ad.duration; 
-            ad.currentTime = widthbar2;
-        });
+        lineInterval = setInterval(updateProgress, 1000);
+        progInterval = setInterval(updateTimer, 1000); 
     } 
     else {
+        clearInterval(lineInterval);
+        clearInterval(progInterval);
         ad.pause();
     }
 
     ttl.classList.toggle('run'); 
     playing.classList.toggle('none');
     pauses.classList.toggle('none');
-    art_img.classList.toggle('round');
-    dur();
+    art_img.classList.toggle('round--pause');
+    displaySongDuration();
 }
 
 function removeEffect() {
@@ -45,60 +57,56 @@ function removeEffect() {
     ttl.classList.remove('run');
     playing.classList.remove('none');
     pauses.classList.add('none');
-    art_img.classList.remove('round');
+    art_img.classList.remove('round--pause');
 }
 
 var x = 0;
 
 function backward() {
-    dur(); 
     x -= 1;
     if (x < 0) { 
         x = artist_name.length - 1;
     }
     removeEffect();
-    songs(x);
+    setSong(x);
 }
 
 function forward() {
-    dur(); 
     x += 1;
     if (x >= artist_name.length) { 
         x = 0;
     }
     removeEffect();
-    songs(x);
+    setSong(x);
 }
 
-function songs(x) {
+function setSong(x) {
     art_name.innerHTML = artist_name[x];
     ttl.innerHTML = artist_title[x];
+    /*const extensions = ['jpg', 'jpeg', 'png'];*/
     art_img.src = `assets/${x}.jpg`;
     ad.src = `assets/${x}.mp3`;
+    ad.addEventListener('loadedmetadata', displaySongDuration);
 }
 
-songs(0);
+setSong(0);
 
-const lines = document.querySelector('.lineChild');
-const progress = document.querySelector('.line');
-const strt = document.querySelector('#start');
-const end = document.querySelector('#end');
-
-function dur() {
-    const dura = ad.duration || 0; 
-    const secdu = Math.floor(dura % 60);
-    const mindu = Math.floor(dura / 60);
-    end.innerHTML = `${mindu}:${secdu < 10 ? '0' : ''}${secdu}`;
+function displaySongDuration() {
+    const duration = ad.duration || 0; 
+    console.log(ad.duration)
+    const seconds = Math.floor(duration % 60);
+    const minutes = Math.floor(duration / 60);
+    end.innerHTML = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
 }
 
-function Prog() {
+function updateTimer() {
     const curtime = ad.currentTime || 0;
     const seccur = Math.floor(curtime % 60);
     const mincur = Math.floor(curtime / 60);
     strt.innerHTML = `${mincur}:${seccur < 10 ? '0' : ''}${seccur}`;
 }
 
-function line() {
+function updateProgress() {
     const widthbar = (ad.currentTime / ad.duration) * 100 || 0; 
     lines.style.width = `${widthbar}%`;
 }
