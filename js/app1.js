5eee767d170b472b3e9489668e4e46383bd635b1
Muhammad emad Hanafy <126643523+muhammademad5@users.noreply.github.com>
2025-01-25 09:41:48 +0200

    app1.js

--- js/app1.js
+++ js/app1.js
@@ -6,39 +6,42 @@ const ttl = document.querySelector('.title');
 const art_img = document.querySelector('#artist');
 const art_name = document.querySelector('#name');
 const playSong = document.querySelector('#playsong');
+const ProgressEvent = document.querySelector('.line'); 
 
 const artist_name = ['muhammad emad', 'ahmad khaled', 'muhammad ali'];
 const artist_title = ['Gods plan', 'The last day', 'The last night'];
 
-playSong.addEventListener('click', effect) 
+playSong.addEventListener('click', effect);
 
-function effect(){
-    if (ad.duration == ad.currentTime){
-        x+=1;
+function effect() {
+    if (ad.duration === ad.currentTime) {
+        x += 1;
         console.log(x);
     }
-    if (!playing.classList.contains('none')){
+
+    if (!playing.classList.contains('none')) {
         ad.play();
-        setInterval(Prog,1000);
-        setInterval(line,1000);
-        ProgressEvent.addEventListener('click',(e)=>{
-            var widthbar2 = (e.offsetX/e.target.clientWidth)*ad.duration;
+        setInterval(Prog, 1000); 
+        setInterval(line, 1000); 
+        ProgressEvent.addEventListener('click', (e) => {
+            const widthbar2 = (e.offsetX / e.target.clientWidth) * ad.duration; 
             ad.currentTime = widthbar2;
-        })
-    }
-    else{
+        });
+    } 
+    else {
         ad.pause();
     }
-    ttl.classList.taggle('run');
+
+    ttl.classList.toggle('run'); 
     playing.classList.toggle('none');
-    pauses.classList.taggle('none');
-    art_img.classList.taggle('round');
+    pauses.classList.toggle('none');
+    art_img.classList.toggle('round');
     dur();
 }
 
-function removeEffect(){
+function removeEffect() {
     ad.pause();
-    ad.currentTime = 0.01;
+    ad.currentTime = 0.01; 
     ttl.classList.remove('run');
     playing.classList.remove('none');
     pauses.classList.add('none');
@@ -47,28 +50,27 @@ function removeEffect(){
 
 var x = 0;
 
-function backward(){
-    dur();
-    x-=1;
+function backward() {
+    dur(); 
+    x -= 1;
+    if (x < 0) { 
+        x = artist_name.length - 1;
+    }
     removeEffect();
     songs(x);
-    if (x <= 0){
-        x=artist_name.length;
-    }
 }
 
-function forward(){
-    dur();
-    x+=1;
+function forward() {
+    dur(); 
+    x += 1;
+    if (x >= artist_name.length) { 
+        x = 0;
+    }
     removeEffect();
     songs(x);
-    if (x >= artist_name.length-1){
-        x=-1;
-    }
 }
 
-function songs(x){
-
+function songs(x) {
     art_name.innerHTML = artist_name[x];
     ttl.innerHTML = artist_title[x];
     art_img.src = `assets/${x}.jpg`;
@@ -82,28 +84,21 @@ const progress = document.querySelector('.line');
 const strt = document.querySelector('#start');
 const end = document.querySelector('#end');
 
-function dur(){
-    var dura = ad.duration;
-    var secdu = Math.floor(dura%60);
-    var mindu = Math.floor(dura/60);
-    if(secdu < 10){
-        secdu = `0${secdu}`;
-    }
-    end.innerHTML = `${mindu}:${secdu}`;
+function dur() {
+    const dura = ad.duration || 0; 
+    const secdu = Math.floor(dura % 60);
+    const mindu = Math.floor(dura / 60);
+    end.innerHTML = `${mindu}:${secdu < 10 ? '0' : ''}${secdu}`;
 }
 
-function Prog(){
-    var curtime = ad.currentTime;
-    var mincur = Math.floor(curtime/60);
-    var seccur = Math.floor(curtime%60);
-    if(seccur < 10){
-        seccur = `0${seccur}`;
-    }
-
-    strt.innerHTML = `${mincur}:${seccur}`;
+function Prog() {
+    const curtime = ad.currentTime || 0;
+    const seccur = Math.floor(curtime % 60);
+    const mincur = Math.floor(curtime / 60);
+    strt.innerHTML = `${mincur}:${seccur < 10 ? '0' : ''}${seccur}`;
 }
 
-function line(){
-    var widthbar = (ad.currentTime/ad.duration)*100;
-    lines.style.width = `${widthbar}%`; 
+function line() {
+    const widthbar = (ad.currentTime / ad.duration) * 100 || 0; 
+    lines.style.width = `${widthbar}%`;
 }
